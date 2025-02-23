834000c9836e9e0352bac56b98e18fd08cf679f9
Jewel Rana <jewelranashepon@gmail.com>
2025-02-23 13:00:23 +0600

    Hero section mobile responsive updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import Particles from "../Backgrounds/Particles/Particles";
 
 const Hero = () => {
   return (
-    <main className="relative min-h-screen">
+    <main className="min-h-screen">
       {/* Particles Background - Covering entire section */}
       <div className="absolute inset-0 z-0">
         <Particles
@@ -24,15 +24,15 @@ const Hero = () => {
       </div>
 
       {/* Content */}
-      <section className="container relative z-10 flex h-screen flex-col items-center justify-center py-20 pb-32 pt-44">
-        <div className="grid grid-cols-1 items-center gap-8 text-white md:grid-cols-2">
+      <section className="container z-10 flex h-screen md:flex-col md:items-center md:justify-center md:py-20 md:pb-32 md:pt-44">
+        <div className="grid grid-cols-1 items-center gap-4 md:gap-8 text-white md:grid-cols-2">
           <div
             data-aos="fade-right"
             data-aos-duration="400"
             data-aos-once="true"
-            className="flex flex-col items-center gap-4 text-center md:items-start md:text-left"
+            className="md:flex md:flex-col items-center md:gap-4 text-center md:items-start md:text-left"
           >
-            <h1 className="mb-3 text-2xl transition-transform duration-500 md:text-5xl">
+            <h1 className="mb-3 mt-8 text-2xl transition-transform duration-500 md:text-5xl">
               <span className="scale-10 transform rounded font-bold text-primary transition-transform duration-500">
                 BIRDS OF EDEN
               </span>{" "}
@@ -52,27 +52,34 @@ const Hero = () => {
                 />
               </span>
             </h1>
-            <p className="text-xl font-light text-black duration-500 dark:text-white">
+            <p className="md:text-xl font-light text-black duration-500 dark:text-white">
               Birds OF EDEN is a cutting-edge software company dedicated to
               transforming ideas into reality through innovative technology
               solutions. Our mission is to empower businesses and individuals
               with state-of-the-art software that enhances efficiency,
               productivity, and user experience.
             </p>
-            <div className="mt-4 md:space-x-4">
+
+            <div className="md:hidden">
+              <CircleServices />
+            </div>
+
+            <div className="grid gap-1 md:flex md:mt-4 md:space-x-4">
               <Link to="/get-started">
-                <button className="mb-2 rounded-md border-2 border-blue-500 bg-blue-500/50 px-8 py-3 text-2xl text-white shadow-md transition-transform duration-300 hover:scale-105 hover:border-blue-700 hover:bg-blue-700">
+                <button className="mb-2 rounded-md border-2 border-blue-500 bg-blue-500/50 px-8 py-3 text-lg md:text-2xl text-white shadow-md transition-transform duration-300 hover:scale-105 hover:border-blue-700 hover:bg-blue-700">
                   Get Started
                 </button>
               </Link>
               <Link to="/learn-more">
-                <button className="rounded-md border-2 border-emerald-500 bg-emerald-500/50 px-8 py-3 text-2xl text-white shadow-md transition-transform duration-300 hover:scale-105 hover:bg-emerald-500">
+                <button className="rounded-md border-2 border-emerald-500 bg-emerald-500/50 px-8 py-3 text-lg md:text-2xl text-white shadow-md transition-transform duration-300 hover:scale-105 hover:bg-emerald-500">
                   Learn More
                 </button>
               </Link>
             </div>
           </div>
-          <CircleServices />
+          <div className="hidden md:block">
+            <CircleServices />
+          </div>
         </div>
       </section>
     </main>
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -3,7 +3,7 @@ import CountUp from "react-countup";
 
 const Service = () => {
   return (
-    <section className="container h-12 md:h-32">
+    <section className="container my-20 h-12 md:h-32">
       <div className="mx-auto my-4 grid w-full -translate-y-10 grid-cols-4 divide-x divide-slate-700 bg-white p-2 shadow-lg ring-blue-400 ring-opacity-75 ring-offset-2 ring-offset-slate-700 transition-all duration-300 hover:ring-4 dark:bg-gray-800 dark:text-white/70 md:max-w-[800px] md:-translate-y-16 md:p-8">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-sm font-bold text-black/80 dark:text-white sm:text-lg md:text-3xl">
