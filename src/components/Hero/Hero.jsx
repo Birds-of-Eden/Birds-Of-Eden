1f095f018b9c31128020d969e74a304261aff810
BoedZisan <stack1@birdsofeden.me>
2025-02-23 15:08:33 +0600

    hero section updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,12 +25,13 @@ const Hero = () => {
 
       {/* Content */}
       <section className="container z-10 flex h-screen md:flex-col md:items-center md:justify-center md:py-20 md:pb-32 md:pt-44">
-        <div className="grid grid-cols-1 items-center gap-4 md:gap-8 text-white md:grid-cols-2">
+        <div className="grid grid-cols-1 items-center gap-4 text-white md:grid-cols-2 md:gap-8">
           <div
             data-aos="fade-right"
             data-aos-duration="400"
             data-aos-once="true"
-            className="md:flex md:flex-col items-center md:gap-4 text-center md:items-start md:text-left"
+            f
+            className="items-center text-center md:flex md:flex-col md:items-start md:gap-4 md:text-left"
           >
             <h1 className="mb-3 mt-8 text-2xl transition-transform duration-500 md:text-5xl">
               <span className="scale-10 transform rounded font-bold text-primary transition-transform duration-500">
@@ -52,7 +53,7 @@ const Hero = () => {
                 />
               </span>
             </h1>
-            <p className="md:text-xl font-light text-black duration-500 dark:text-white">
+            <p className="font-light text-black duration-500 dark:text-white md:text-xl">
               Birds OF EDEN is a cutting-edge software company dedicated to
               transforming ideas into reality through innovative technology
               solutions. Our mission is to empower businesses and individuals
@@ -64,14 +65,14 @@ const Hero = () => {
               <CircleServices />
             </div>
 
-            <div className="grid gap-1 md:flex md:mt-4 md:space-x-4">
+            <div className="grid gap-1 md:mt-4 md:flex md:space-x-4">
               <Link to="/get-started">
-                <button className="mb-2 rounded-md border-2 border-blue-500 bg-blue-500/50 px-8 py-3 text-lg md:text-2xl text-white shadow-md transition-transform duration-300 hover:scale-105 hover:border-blue-700 hover:bg-blue-700">
+                <button className="mb-2 rounded-md border-2 border-blue-500 bg-blue-500/50 px-8 py-3 text-lg text-white shadow-md transition-transform duration-300 hover:scale-105 hover:border-blue-700 hover:bg-blue-700 md:text-2xl">
                   Get Started
                 </button>
               </Link>
               <Link to="/learn-more">
-                <button className="rounded-md border-2 border-emerald-500 bg-emerald-500/50 px-8 py-3 text-lg md:text-2xl text-white shadow-md transition-transform duration-300 hover:scale-105 hover:bg-emerald-500">
+                <button className="rounded-md border-2 border-emerald-500 bg-emerald-500/50 px-8 py-3 text-lg text-white shadow-md transition-transform duration-300 hover:scale-105 hover:bg-emerald-500 md:text-2xl">
                   Learn More
                 </button>
               </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,20 +17,18 @@ import DronePage from "./DronePage";
 
 import BounceCards from "../components/BounceCards/BounceCards";
 
-const images = [
-  "https://picsum.photos/400/400?grayscale",
-  "https://picsum.photos/500/500?grayscale",
-  "https://picsum.photos/600/600?grayscale",
-  "https://picsum.photos/700/700?grayscale",
-  "https://picsum.photos/300/300?grayscale",
-];
+import office_1 from "../assets/office_1.png";
+import office_2 from "../assets/office_2.png";
+import office_3 from "../assets/office_3.png";
+
+const images = [office_1, office_2, office_1];
 
 const transformStyles = [
-  "rotate(5deg) translate(-150px)",
-  "rotate(0deg) translate(-70px)",
-  "rotate(-5deg)",
-  "rotate(5deg) translate(70px)",
-  "rotate(-5deg) translate(150px)",
+  // "rotate(5deg) translate(-350px)",
+  "rotate(5deg) translate(-180px)",
+  "rotate(0deg)",
+  "rotate(-5deg) translate(180px)",
+  // "rotate(-10deg) translate(150px)",
 ];
 
 const Home = () => {
@@ -50,24 +48,50 @@ const Home = () => {
       </div>
 
       <div style={{ height: "700px", position: "relative" }} className="pb-40">
-        <h1 className="heading">Our Partners</h1>
+        <h1 className="heading3 pt-3">Our Partners</h1>
         <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
       </div>
 
       {/* Add content for the home page */}
-      {/* <div>
-        <BounceCards
-          className="custom-bounceCards"
-          images={images}
-          containerWidth={500}
-          containerHeight={250}
-          animationDelay={1}
-          animationStagger={0.08}
-          easeType="elastic.out(1, 0.5)"
-          transformStyles={transformStyles}
-          enableHover={true}
-        />
-      </div> */}
+      {/* BounceCards Section with Title and Background */}
+      <div
+        style={{
+          backgroundColor: "#222D40", // Light gray background
+          padding: "50px 0",
+          textAlign: "center",
+        }}
+      >
+        <h2
+          style={{
+            // color: "#fff",
+            fontSize: "2.5em",
+            marginBottom: "20px",
+            fontFamily: "Arial, sans-serif",
+          }}
+          className="heading2 pb-10"
+        >
+          Our Team Moments
+        </h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <BounceCards
+            className="custom-bounceCards"
+            images={images}
+            containerWidth={500}
+            containerHeight={250}
+            animationDelay={1}
+            animationStagger={0.09}
+            easeType="elastic.out(1, 0.5)"
+            transformStyles={transformStyles}
+            enableHover={true}
+          />
+        </div>
+      </div>
     </div>
   );
 };
