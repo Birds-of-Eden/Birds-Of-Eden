6f08b5cad78be3803821568643b4b2b1ca135277
tauhid97k <tauhid.coder@gmail.com>
2025-02-25 17:27:08 +0600

    hero section update and particles effect to all pages.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,25 +3,10 @@ import { Link } from "react-router-dom";
 // import Banner from "../../assets/undraw_real_time_sync_re_nky7.svg";
 import CircleServices from "./circleServices";
 import Typewriter from "typewriter-effect";
-import Particles from "../Backgrounds/Particles/Particles";
 
 const Hero = () => {
   return (
     <main className="flex flex-col pt-[100px] lg:pt-0">
-      {/* Particles Background - Covering entire section */}
-      <div className="fixed inset-0 z-0">
-        <Particles
-          particleColors={["#ffffff", "#ffffff"]}
-          particleCount={200}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={100}
-          moveParticlesOnHover={true}
-          alphaParticles={false}
-          disableRotation={false}
-        />
-      </div>
-
       {/* Content */}
       <section className="container">
         <div className="grid grid-cols-1 items-center gap-4 text-white md:gap-8 lg:h-[calc(100vh-112px)] lg:grid-cols-2">
@@ -29,9 +14,9 @@ const Hero = () => {
             data-aos="fade-right"
             data-aos-duration="400"
             data-aos-once="true"
-            className="flex flex-col items-start gap-4 text-center lg:text-left"
+            className="flex flex-col items-start gap-4 text-left"
           >
-            <h1 className="text-center text-4xl transition-transform duration-500 xl:text-5xl">
+            <h1 className="text-4xl transition-transform duration-500 xl:text-5xl">
               <span className="scale-10 mb-1 block transform rounded font-bold text-primary transition-transform duration-500">
                 BIRDS OF EDEN
               </span>
@@ -58,7 +43,7 @@ const Hero = () => {
               with state-of-the-art software that enhances efficiency,
               productivity, and user experience.
             </p>
-            <div className="mt-4 flex w-full justify-center gap-4">
+            <div className="mt-4 flex w-full justify-start gap-4">
               <Link
                 to="/get-started"
                 className="mb-2 rounded-md border-2 border-blue-500 bg-blue-500/50 px-4 py-2 text-lg text-black shadow-md transition-transform duration-300 hover:scale-105 hover:border-blue-700 hover:bg-blue-700 dark:text-white xl:px-8 xl:py-3 xl:text-2xl"
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,10 +3,24 @@ import Navbar from "../components/Navbar/Navbar3";
 import Footer from "../components/Footer/Footer";
 // import Navbar2 from "../components/navbar/Navbar2";
 import SplashCursor from "../components/SplashCursor/SplashCursor";
+import Particles from "../components/Backgrounds/Particles/Particles";
 
 const Layout = ({ children }) => {
   return (
     <div className="dark:bg-slate-900 dark:text-white">
+      {/* Particles Background - Covering entire section */}
+      <div className="fixed inset-0 z-0">
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
       <Navbar />
       <SplashCursor /> {/* Directly using Splash Cursor */}
       <main>{children}</main>
